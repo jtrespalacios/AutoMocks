bfcaf87633311c7195d54691633e8949953d2c43
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2017-04-06 10:58:53 -0700

    Initial Commit

---
+++ AutoMocks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoMocks
+//
+//  Created by Jeff Trespalacios on 4/6/17.
+//  Copyright © 2017 Jeff Trespalacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

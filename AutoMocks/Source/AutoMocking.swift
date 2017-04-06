10014184e5d99502ad37b28ff9274f436769b88a
Jeff Trespalacios <jeff.trespalacios@gmail.com>
2017-04-06 11:18:32 -0700

    add setup files

---
+++ AutoMocks/Source/AutoMocking.swift
@@ -0,0 +1,15 @@
+//
+//  AutoMocking.swift
+//  AutoMocks
+//
+//  Created by Jeff Trespalacios on 4/6/17.
+//  Copyright © 2017 Jeff Trespalacios. All rights reserved.
+//
+
+import Foundation
+
+protocol AutoCases {}
+protocol AutoDiffable {}
+protocol AutoEquatable {}
+protocol AutoHashable {}
+protocol AutoMockable {}
---
+++ monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Example Template Source: https://github.com/krzysztofzablocki/Sourcery#examples
+
+~/Applications/Sourcery.app/Contents/MacOS/Sourcery AutoMocks/Source/ Templates AutoMocks/Generated/ --watch --verbose
